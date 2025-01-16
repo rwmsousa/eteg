80562ddcdcd209f1cbee73b46b4accd977b8202e
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-16 16:05:38 -0300

    feat: add user management functionality to Admin module with CRUD operations

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Get,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { User } from '../entities/user.entity';
 
 @Controller('admin')
 export class AdminController {
@@ -8,5 +17,30 @@ export class AdminController {
   @Post('login')
   login(@Body() loginData: any) {
     return this.adminService.login(loginData);
+  }
+
+  @Post('register')
+  registerUser(@Body() userData: User) {
+    return this.adminService.registerUser(userData);
+  }
+
+  @Get('users')
+  listUsers() {
+    return this.adminService.listUsers();
+  }
+
+  @Get('users/:id')
+  getUser(@Param('id') id: number) {
+    return this.adminService.getUser(id);
+  }
+
+  @Put('users/:id')
+  updateUser(@Param('id') id: number, @Body() userData: User) {
+    return this.adminService.updateUser(id, userData);
+  }
+
+  @Delete('users/:id')
+  deleteUser(@Param('id') id: number) {
+    return this.adminService.deleteUser(id);
   }
 }
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '../entities/user.entity';
 
 @Injectable()
 export class AdminService {
+  private users: User[] = [];
+
   login(loginData: any) {
     // Logic to authenticate admin user
     console.log('Admin login', loginData);
+  }
+
+  registerUser(userData: User) {
+    this.users.push(userData);
+    return userData;
+  }
+
+  listUsers() {
+    return this.users;
+  }
+
+  getUser(id: number) {
+    return this.users.find((user) => user.id === id);
+  }
+
+  updateUser(id: number, userData: User) {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index !== -1) {
+      this.users[index] = userData;
+      return userData;
+    }
+    return null;
+  }
+
+  deleteUser(id: number) {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index !== -1) {
+      const deletedUser = this.users.splice(index, 1);
+      return deletedUser[0];
+    }
+    return null;
   }
 }
