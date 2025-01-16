4a6a3508ab62a8abdd87ae6886efbba9a2a16b9c
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-16 13:21:37 -0300

    feat: add ClientsModule and AdminModule to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule } from './clients/clients.module';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [ClientsModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
