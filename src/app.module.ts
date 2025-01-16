ea04f81427ff6b4a9f488db547a978fd32136b93
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-16 16:04:27 -0300

    feat: configure PostgreSQL connection and add Migrations module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
 import { AdminModule } from './admin/admin.module';
+import { MigrationsModule } from './migrations/migrations.module';
 
 @Module({
-  imports: [ClientsModule, AdminModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: process.env.NODE_ENV === 'development',
+      autoLoadEntities: true,
+    }),
+    ClientsModule,
+    AdminModule,
+    MigrationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
