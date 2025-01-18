053f72e6abe020adce9330dc683ee40d91f91d72
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-18 19:42:59 -0300

    feat: enhance AuthMiddleware to validate JWT and retrieve user details, add LoginData DTO for login credentials

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,7 +1,12 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  Injectable,
+  NestMiddleware,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import { User } from '../entities/user.entity';
 import * as jwt from 'jsonwebtoken';
+import { UserService } from '../user/user.service';
+import { User } from '../entities/user.entity';
 
 interface CustomRequest extends Request {
   user?: User;
@@ -9,16 +14,29 @@ interface CustomRequest extends Request {
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  use(req: CustomRequest, res: Response, next: NextFunction) {
-    const token = req.headers['authorization']?.split(' ')[1];
-    if (token) {
-      try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET) as User;
-        req.user = decoded;
-      } catch (err) {
-        console.error('Invalid token', err);
-      }
+  constructor(private readonly userService: UserService) {}
+
+  async use(req: CustomRequest, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      throw new UnauthorizedException('Authorization header missing');
     }
-    next();
+
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+      throw new UnauthorizedException('Token missing');
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET) as any;
+      const user = await this.userService.getUserById(decoded.sub);
+      if (!user) {
+        throw new UnauthorizedException('User not found');
+      }
+      req.user = user;
+      next();
+    } catch {
+      throw new UnauthorizedException('Invalid token');
+    }
   }
 }
