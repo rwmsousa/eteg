baf203213bef5d1f5fd58c194602005007c5831f
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-18 12:55:29 -0300

    feat: migrate database configuration to data-source.ts and remove obsolete files

---
+++ src/data-source.ts
@@ -0,0 +1,17 @@
+import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/migrations/*{.ts,.js}'],
+  synchronize: process.env.DATABASE_SYNCHRONIZE === 'true',
+  logging: Boolean(process.env.DATABASE_LOGGING),
+});
