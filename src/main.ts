8256a4dbd9f271e37af7876c582cede86631d956
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-19 10:55:36 -0300

    feat: add Swagger documentation setup and options for the API

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { NestExpressApplication } from '@nestjs/platform-express';
 import * as dotenv from 'dotenv';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 import SeederService from './seeder/seeder.service';
 import { AppDataSource } from './data-source';
+import { SwaggerDocumentOptions } from './swagger/swagger-document-options';
 
 dotenv.config();
 
@@ -13,6 +15,35 @@ async function bootstrap() {
   await AppDataSource.runMigrations();
   const seeder = app.get(SeederService);
   await seeder.seed();
+
+  const config = new DocumentBuilder()
+    .setTitle('Eteg API')
+    .setDescription('Test Technique Eteg')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const swaggerOptions: SwaggerDocumentOptions = {
+    include: [],
+    extraModels: [],
+    ignoreGlobalPrefix: false,
+    deepScanRoutes: true,
+    operationIdFactory: (
+      controllerKey: string,
+      methodKey: string,
+      version?: string,
+    ) => `${controllerKey}_${methodKey}_${version}`,
+    linkNameFactory: (
+      controllerKey: string,
+      methodKey: string,
+      fieldKey: string,
+    ) => `${controllerKey}_${methodKey}_from_${fieldKey}`,
+    autoTagControllers: true,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, swaggerOptions);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
---
+++ src/swagger/swagger-document-options.ts
@@ -0,0 +1,26 @@
+import { DocumentBuilder } from '@nestjs/swagger';
+
+export const swaggerDocumentOptions = new DocumentBuilder()
+  .setTitle('Eteg API')
+  .setDescription('Test Technique Eteg')
+  .setVersion('1.0')
+  .addTag('nestjs')
+  .build();
+
+export interface SwaggerDocumentOptions {
+  include?: Array<new (...args: any[]) => any>;
+  extraModels?: Array<new (...args: any[]) => any>;
+  ignoreGlobalPrefix?: boolean;
+  deepScanRoutes?: boolean;
+  operationIdFactory?: (
+    controllerKey: string,
+    methodKey: string,
+    version?: string,
+  ) => string;
+  linkNameFactory?: (
+    controllerKey: string,
+    methodKey: string,
+    fieldKey: string,
+  ) => string;
+  autoTagControllers?: boolean;
+}
