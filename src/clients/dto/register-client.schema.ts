b268b75bbc604c25adec3fbaadbffbe0b196dd02
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-19 14:52:33 -0300

    chore: remove obsolete configuration and source files

--- src/clients/dto/register-client.schema.ts
+++
@@ -1,9 +0,0 @@
-import * as Joi from 'joi';
-
-export const registerClientSchema = Joi.object({
-  name: Joi.string().min(3).max(50).required(),
-  cpf: Joi.string().length(11).required(),
-  email: Joi.string().email().required(),
-  color: Joi.string().min(3).max(20).required(),
-  annotations: Joi.string().allow('').optional(),
-});
--- src/user/dto/register-user.schema.ts
+++
@@ -1,8 +0,0 @@
-import * as Joi from 'joi';
-
-export const registerUserSchema = Joi.object({
-  username: Joi.string().min(3).max(30).required(),
-  password: Joi.string().min(6).required(),
-  email: Joi.string().email().required(),
-  role: Joi.string().valid('user', 'admin').optional(),
-});
